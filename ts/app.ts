fe556eaeb93eb8dd5b480aa83ebac65455773f2a
kang9kang9 <kang9kang9@deephigh.net>
2022-03-11 10:35:33 +0900

    위치변경

--- ts/app.ts
+++ ts/app.ts
@@ -928,8 +928,6 @@ let myObj = {size: 10, label: "Size 10 Object"}
 
 
 
-
-
 function printLabels(labeledObj: LabeledValue){
     console.log(labeledObj.label)
 }
@@ -937,25 +935,25 @@ function printLabels(labeledObj: LabeledValue){
     let myObjs = { size: 10, label: "Size 11 object"}
     printLabel(myObjs)
 
-    interface SquareConfig {
+interface SquareConfig {
         color?: string
         width?: number
-    }
+}
     
 
 
 
 
-    function createSquare(config: SquareConfig) : {color: string, area:number} {
-        let newSquare = {color: 'white', area: 100}
-        if (config.color) {
-            newSquare.color = config.color
-        }
-        if (config.width) {
-            newSquare.area = config.width * config.width
-        }
-        return newSquare
+function createSquare(config: SquareConfig) : {color: string, area:number} {
+    let newSquare = {color: 'white', area: 100}
+    if (config.color) {
+        newSquare.color = config.color
     }
+    if (config.width) {
+        newSquare.area = config.width * config.width
+    }
+        return newSquare
+}
 
     let mySquare = createSquare({color: "black"})
     console.log(mySquare)
