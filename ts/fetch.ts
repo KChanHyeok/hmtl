af91aafaf57c581d18e5634eaa35a710fb77f2b5
kang9kang9 <kang9kang9@deephigh.net>
2022-03-18 15:56:57 +0900

    정리

--- ts/fetch.ts
+++ ts/fetch.ts
@@ -5,7 +5,7 @@ const RESPONSE_TYPE = 'json'
 class APICaller {
     constructor() {}
 
-    _mergeUrl(url, params = null) {
+    _mergeUrl(url:string, params = null) {
         const defaultUrl = DEFAULT_URL[DEFAULT_URL.length - 1] === '/' ? DEFAULT_URL.substr(0, DEFAULT_URL.length - 1) : DEFAULT_URL
         url = url[0] === '/' ? url.substr(1) : url
 
@@ -22,12 +22,12 @@ class APICaller {
         return retValue
     }
 
-    async get(url, params){
+    async get(url:string, params:null){
         const response = await fetch(this._mergeUrl(url, params))
         return response
     }
 
-    async post(url, data, params){
+    async post(url:string, data:string, params:null){
         const response = await fetch(this._mergeUrl(url, params), {
             method: 'POST',
             headers: {'Content-type': `application/${RESPONSE_TYPE}; charset=UTF-8`}, // application/json, application/x-www-urlencoded
@@ -36,7 +36,7 @@ class APICaller {
         return response
     }
 
-    async put(url, data, params){
+    async put(url:string, data:string, params:null){
         const response = await fetch(this._mergeUrl(url, params), {
             method: 'PUT',
             body: JSON.stringify(data),
@@ -44,13 +44,13 @@ class APICaller {
         })
         return response 
     }
-    async delete(url, params){
+    async delete(url:string, params:null){
         const response = await fetch(this._mergeUrl(url, params), {
             method: 'DELETE'
         })
         return response
     }
-    async patch(url, data, params){
+    async patch(url:string, data:string, params:null){
         const response = await fetch(this._mergeUrl(url, params), {
             method: 'PATCH',
             body: JSON.stringify(data),
